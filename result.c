0df6e0f4d4d9272d3dfe194a268ad7ea7f00df5e
Priyanshu kumar <153292007+Priyanshu0810@users.noreply.github.com>
2023-12-09 10:59:37 +0530

    Update result.c

--- result.c
+++ result.c
@@ -1,1 +1,21 @@
-
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    float a,b,c,d,e,sum;
+    float avg;
+    clrscr();
+  
+    printf("enter first sub number a,b,c,d,e\n");
+    scanf("%f %f %f %f%f",&a,&b,&c,&d,&e);
+  
+    printf("\n the sum of all five num is %f ",a+b+c+d+e);
+  
+    sum=a+b+c+d+e;
+  
+    avg=(sum/5);
+  
+    printf("\n the avg is %f",avg);
+  
+    getch();
+}
